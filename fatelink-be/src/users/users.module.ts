50c71b763c9b0180605066278498e703044650db
peggy2402 <chien24022003@gmail.com>
2026-04-03 15:05:46 +0700

    fixed: import JwtModule into UserModule yet

--- fatelink-be/src/users/users.module.ts
+++ fatelink-be/src/users/users.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({}) // cấp phép sử dụng JwtService cho UsersModule
   ],
   providers: [UsersService],
   exports: [UsersService], // This line is crucial
