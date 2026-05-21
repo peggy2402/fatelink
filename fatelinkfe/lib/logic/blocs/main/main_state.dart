bf83ead57f01e93ec89e69e4ad7c41e7b08df9a3
peggy2402 <tranvanchien24022003@gmail.com>
2026-05-22 00:41:53 +0700

    fix: return screen previous, refactor profile screen

--- fatelinkfe/lib/logic/blocs/main/main_state.dart
+++ fatelinkfe/lib/logic/blocs/main/main_state.dart
@@ -2,13 +2,20 @@ import 'package:equatable/equatable.dart';
 
 class MainState extends Equatable {
   final int selectedIndex;
+  final List<int> tabHistory; // Thêm mảng lưu lịch sử
 
-  const MainState({this.selectedIndex = 0});
+  const MainState({
+    this.selectedIndex = 0,
+    this.tabHistory = const [0],
+  });
 
-  MainState copyWith({int? selectedIndex}) {
-    return MainState(selectedIndex: selectedIndex ?? this.selectedIndex);
+  MainState copyWith({int? selectedIndex, List<int>? tabHistory}) {
+    return MainState(
+      selectedIndex: selectedIndex ?? this.selectedIndex,
+      tabHistory: tabHistory ?? this.tabHistory,
+    );
   }
 
   @override
-  List<Object> get props => [selectedIndex];
+  List<Object> get props => [selectedIndex, tabHistory];
 }
